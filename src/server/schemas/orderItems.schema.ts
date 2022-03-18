72d6f1afb6c68d59b8e9d0e74ed2cd194d07196b
mekdes <mbayeleyegn@alphawork.tech>
2022-03-17 19:28:55 -0700

    feat(server):create orderItems schema

---
+++ src/server/schemas/orderItems.schema.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+import { orderItems } from './../../shared/models/orderItems.model';
+
+const { Schema, model } = mongoose
+
+const orderItemSchema = new Schema<orderItems>({
+    name: {
+        type:Number,required:true
+    },
+    quantity: {
+        type:Number,required:true
+    },
+    price:{type:Number,required:true}
+})
+
+export const OrderItemModel=model('OrderItem',orderItemSchema)
