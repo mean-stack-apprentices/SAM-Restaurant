f49481e4565179d2b688338745032fdc36f80cd2
saba <sabera@alphaworks.tech>
2022-03-21 13:11:36 -0700

    freat:(routing) add menu and home page routes

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuListComponent } from './pages/menu-list/menu-list.component';
 import { PageUsersComponent } from './pages/page-users/page-users.component';
 
 const routes: Routes = [
-  {path: 'users', component: PageUsersComponent}
+  {path: 'users', component: PageUsersComponent},
+  {path:'menu', component:MenuListComponent},
+  {path:'',component:HomeComponent}
+
 ];
 
 @NgModule({
