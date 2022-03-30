46a7aa0d82e349837d77273be4c6d8d18e457091
saba <sabera@alphaworks.tech>
2022-03-30 11:19:51 -0700

    feat(client) display menu items

--- src/client/src/app/pages/menu-list/menu-list.component.ts
+++ src/client/src/app/pages/menu-list/menu-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CategoryService } from 'src/app/services/category.service';
+import { MenuItemsService } from 'src/app/services/menu-items.service';
+import { Category } from '../../../../../shared/models/category.model';
+import { MenuItems } from '../../../../../shared/models/menuItems.model';
 
 @Component({
   selector: 'app-menu-list',
@@ -7,12 +12,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./menu-list.component.scss']
 })
 export class MenuListComponent implements OnInit {
+  menuItems$:Observable< MenuItems[]>
+  // category$: Observable<Category[]>
+  categories: Category[] = [];
 
-  constructor(private router: Router) { }
+  constructor
+  (private router: Router,
+  private menuItemService: MenuItemsService,
+  private categoryService: CategoryService
+    ) {
+     this.menuItems$= this.menuItemService.getMenuItems()
+
+     }
 
   ngOnInit(): void {
   }
-
 
 
 }
