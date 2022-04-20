6f1c3430450729f6cd9d1885a738d4caba6f70da
Angel <angelcruz981@yahoo.com>
2022-04-20 11:23:52 -0700

    feat(login): create component, enable simple form

---
+++ src/client/src/app/components/user-login/user-login.component.ts
@@ -0,0 +1,37 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.scss']
+})
+export class UserLoginComponent implements OnInit {
+
+  signInForm: FormGroup
+  constructor(
+    private userService: UserService,
+    private store: Store,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.signInForm = this.fb.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required],
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  signIn() {
+    console.log(this.signInForm.value)
+    // this.userService.login(this.signInForm.value).subscribe();
+    this.signInForm.reset();
+  }
+
+}
