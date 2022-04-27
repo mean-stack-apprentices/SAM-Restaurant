712e453e552f4f918f35817da1379c69205ce973
mekdes <mbayeleyegn@alphawork.tech>
2022-04-26 17:09:08 -0700

    ref(server):add .js

--- src/shared/models/cart.model.ts
+++ src/shared/models/cart.model.ts
@@ -1,5 +1,5 @@
 import * as mongoose from 'mongoose';
-import { MenuItems } from './menuItems.model';
+import { MenuItems } from './menuItems.model.js';
 import { User } from './user.model.js';
 export interface Cart{
     _id?:{type: mongoose.Types.ObjectId}
