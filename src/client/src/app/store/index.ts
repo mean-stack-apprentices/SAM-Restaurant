d20a712543a1000d5303537df03a09e505035e24
saba <sabera@alphaworks.tech>
2022-04-29 09:40:24 -0700

    feat(index.ts): add cart & menuItem states

--- src/client/src/app/store/index.ts
+++ src/client/src/app/store/index.ts
@@ -7,16 +7,20 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import * as fromUser from './reducers/user/user.reducer';
-
-
+import * as fromMenuItem from './reducers/menu-item/menu-item.reducer';
+import * as fromCart from './reducers/cart/cart.reducer';
 export interface AppState {
 
   [fromUser.userFeatureKey]: fromUser.State;
+  [fromCart.cartFeatureKey]: fromCart.State;
+  [fromMenuItem.menuItemFeatureKey]: fromMenuItem.State;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
 
   [fromUser.userFeatureKey]: fromUser.reducer,
+  [fromCart.cartFeatureKey]: fromCart.reducer,
+  [fromMenuItem.menuItemFeatureKey]: fromMenuItem.reducer,
 };
 
 
