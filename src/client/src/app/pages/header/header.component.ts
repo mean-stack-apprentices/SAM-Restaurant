db4927497d90f8e3a7cfe6bac7cee94677929da1
Angel <angelcruz981@yahoo.com>
2022-05-03 16:11:25 -0700

    feat(header): Nav now shows when user is logged in

--- src/client/src/app/pages/header/header.component.ts
+++ src/client/src/app/pages/header/header.component.ts
@@ -4,6 +4,10 @@ import { timingSafeEqual } from 'crypto';
 import { Observable } from 'rxjs';
 import { CategoryService } from 'src/app/services/category.service';
 import { Category } from '../../../../../shared/models/category.model';
+import { User } from '../../../../../shared/models/user.model';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store';
+import { loggedInUserSelector } from 'src/app/store/selectors/user/user.selectors';
 
 // import { MatMenuTrigger } from '@angular/material/menu/matMenu';
 
@@ -14,9 +18,12 @@ import { Category } from '../../../../../shared/models/category.model';
 })
 export class HeaderComponent implements OnInit {
   category$: Observable<Category[]>;
+  loggedInUser$!: Observable<User | null>;
   isOpen = false;
-  constructor(public router: Router, private categoryService: CategoryService) {
+  constructor(public router: Router, private categoryService: CategoryService, private store: Store<AppState>) {
     this.category$ = this.categoryService.getCategories();
+    this.loggedInUser$ = this.store.select(loggedInUserSelector)
+
   }
 
   ngOnInit(): void {}
