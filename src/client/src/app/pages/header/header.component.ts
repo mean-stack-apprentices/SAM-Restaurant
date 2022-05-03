9e03d1e0542887f0e9c0eb3cd78278ea2e2a00b2
saba <sabera@alphaworks.tech>
2022-05-03 10:11:44 -0700

    feat(client): add cart length

--- src/client/src/app/pages/header/header.component.ts
+++ src/client/src/app/pages/header/header.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { timingSafeEqual } from 'crypto';
 import { Observable } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
 import { CategoryService } from 'src/app/services/category.service';
 import { Category } from '../../../../../shared/models/category.model';
+import { MenuItems } from '../../../../../shared/models/menuItems.model';
 
 // import { MatMenuTrigger } from '@angular/material/menu/matMenu';
 
@@ -13,10 +15,13 @@ import { Category } from '../../../../../shared/models/category.model';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  cart: MenuItems[] =[];
   category$: Observable<Category[]>;
   isOpen = false;
-  constructor(public router: Router, private categoryService: CategoryService) {
+  constructor(public router: Router, private categoryService: CategoryService,private cartService:CartService) {
     this.category$ = this.categoryService.getCategories();
+    this.cart = this.cartService.cart
+ console.log(this.cart)
   }
 
   ngOnInit(): void {}
